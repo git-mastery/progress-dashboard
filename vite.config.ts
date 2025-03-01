e664c06997b62dc179fff300d37d81664d0a58c6
Jiahao, Woo <woojiahao1234@gmail.com>
2025-03-01 22:16:37 +0800

    Remove conditional base

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-const base = process.env.NODE_ENV === "production" ? "/progress-dashboard/" : ""
-console.log(process.env.NODE_ENV)
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base,
+  base: "/progress-dashboard/",
 })
