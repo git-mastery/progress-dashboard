a4a8cef1c10dc75ec806a9003d8cb5beca08b80f
Jiahao, Woo <woojiahao1234@gmail.com>
2025-07-19 18:47:51 +0800

    Clean up wordings

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -44,32 +44,30 @@ function Dashboard() {
   }, [userProgress, isUserProgressLoading])
 
   const {
-    data: problemSets,
+    data: exercises,
     isLoading: isProblemSetsLoading,
   } = useGetExercisesQuery();
 
-  const problemSetGroups = useMemo(() => {
-    if (isProblemSetsLoading || problemSets == null) {
+  const exerciseGroups = useMemo(() => {
+    if (isProblemSetsLoading || exercises == null) {
       return new Map<string, Exercise[]>();
     }
 
     const repoGroups = new Map<string, Exercise[]>();
-    for (const repo of problemSets) {
-      for (const tag of repo.tags) {
-        if (tag !== "problem-set") {
-          if (parsedUserProgress.has(repo.exercise_name)) {
-            if (!repoGroups.has(tag)) {
-              repoGroups.set(tag, [])
-            }
-            repoGroups.get(tag)!.push(repo)
+    for (const exercise of exercises) {
+      for (const tag of exercise.tags) {
+        if (parsedUserProgress.has(exercise.exercise_name)) {
+          if (!repoGroups.has(tag)) {
+            repoGroups.set(tag, [])
           }
+          repoGroups.get(tag)!.push(exercise)
         }
       }
     }
 
     const sortedGroups = new Map([...repoGroups.entries()].sort())
     return sortedGroups
-  }, [problemSets, isProblemSetsLoading, parsedUserProgress])
+  }, [exercises, isProblemSetsLoading, parsedUserProgress])
 
   const refreshUserProgress = useCallback(async () => {
     await refetchUserProgress()
@@ -89,8 +87,8 @@ function Dashboard() {
           </div>
           <button type="button" className="hover:cursor-pointer" onClick={refreshUserProgress}><MdOutlineRefresh size={24} color="text-gray-500" /></button>
         </div>
-        <p className="text-gray-700 font-semibold">Find your progress for the various Git Mastery problem sets.</p>
-        <p className="text-gray-700">To view all problem sets, visit the <a className="text-blue-800 underline" href="https://github.com/git-mastery/problems-directory">problems directory</a>.</p>
+        <p className="text-gray-700 font-semibold">Find your progress for the various Git Mastery exercises.</p>
+        <p className="text-gray-700">To view all exercises, visit the <a className="text-blue-800 underline" href="https://git-mastery.github.io/exercises">exercises directory</a>.</p>
       </div>
       <div>
         {(isUserLoading || isUserProgressLoading || isUserProgressRefetching || isProblemSetsLoading) ? (
@@ -104,13 +102,13 @@ function Dashboard() {
               <Link to="/" className="hover:cursor-pointer border-1 border-red-700 bg-red-700 text-white rounded-sm px-4 py-2 font-semibold">← Return to search</Link>
             </div>
           ) : (
-            problemSetGroups.size === 0 ? (
+            exerciseGroups.size === 0 ? (
               <div className="text-center">
-                <p className="mb-4">You have not completed any problem sets yet</p>
-                <a href="https://github.com/git-mastery/problems-directory" target="_blank" className="hover:cursor-pointer border-1 border-blue-800 bg-blue-800 text-white rounded-sm px-4 py-2 font-semibold">Go to problems directory ↗</a>
+                <p className="mb-4">You have not completed any exercises yet</p>
+                <a href="https://git-mastery.github.io/exercises" target="_blank" className="hover:cursor-pointer border-1 border-blue-800 bg-blue-800 text-white rounded-sm px-4 py-2 font-semibold">Go to exercises directory ↗</a>
               </div>
             ) : (
-              Array.from(problemSetGroups)
+              Array.from(exerciseGroups)
                 .map(([key, value]) => {
                   return (
                     <div key={key} className="not-last:mb-4">
@@ -127,7 +125,7 @@ function Dashboard() {
                             .filter(exercise => parsedUserProgress.has(exercise.exercise_name))
                             .map((exercise, idx) => (
                               <tr key={idx}>
-                                <td className="border border-gray-300 px-4 py-2 text-left"><a target="_blank" href={`https://git-mastery.github.io/exercises/${exercise.exercise_name.replace("_", "-")}`}><code className="underline text-blue-800">{exercise.exercise_name}</code></a></td>
+                                <td className="border border-gray-300 px-4 py-2 text-left"><a target="_blank" href={`https://git-mastery.github.io/exercises/${exercise.exercise_name.replace("-", "_")}`}><code className="underline text-blue-800">{exercise.exercise_name}</code></a></td>
                                 <td className="border border-gray-300 px-4 py-2 text-left">{parsedUserProgress.get(exercise.exercise_name)}</td>
                               </tr>
                             ))}
