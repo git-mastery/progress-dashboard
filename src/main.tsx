6043e150fd821615892b4fd06cf79ba2b00a173f
jovnc <95868357+jovnc@users.noreply.github.com>
2026-02-03 18:26:38 +0800

    Add catch all route to redirect to index route

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from 'react-query'
-import { HashRouter, Route, Routes } from 'react-router'
+import { HashRouter, Navigate, Route, Routes } from 'react-router'
 import '@/styles/index.css'
 import App from '@/pages'
 import Dashboard from '@/pages/dashboard/(username)'
@@ -15,6 +15,7 @@ createRoot(document.getElementById('root')!).render(
         <Routes>
           <Route index element={<App />} />
           <Route path="dashboard/:username" element={<Dashboard />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </HashRouter>
     </QueryClientProvider>
